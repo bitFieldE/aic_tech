cd04e01b0c27b59aebfff377f1b0fedd12cd0ceb
亀島　隆平 <ryuhei@kamejimaryuuhei-no-MacBook-Air.local>
2020-01-25 03:35:27 +0900

    CircleCI 編集

--- spec/requests/relationships_spec.rb
+++ spec/requests/relationships_spec.rb
@@ -6,9 +6,13 @@ RSpec.describe "Relationships", type: :request do
     before do
       allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return(user_id: user.id)
     end
-    it "works! (now write some real specs)" do
+    it 'works! (now write some real specs)' do
       get relationships_index_path
       expect(response).to have_http_status(200)
+    end
+
+    it '' do
+      
     end
   end
 end
